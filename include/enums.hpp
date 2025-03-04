eda9eb3534e8f51507cc8e8329302e47ce385255
Iury Gama <Iuryempregos@gmail.com>
2025-03-04 17:43:14 -0300

    Creating a new CustomErrors class with inheritance

--- include/enums.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-enum class ErrorType
-{
-    UNKNOWN_ERROR,
-    FRAME_IS_TOO_SHORT,
-    CHECKSUM_DOES_NOT_MATCH,
-    INVALID_ADDRESS,
-};
--- include/utils.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "custom_errors.hpp"
-#include <grpcpp/grpcpp.h>
-
-namespace ees
-{
-    auto map_exception_to_error(const exceptions::CustomException &error ) -> grpc::Status;
-}
